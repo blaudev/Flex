1d20e7a9f32e424042d7bd4630239b3e0c671021
Daniel Peiró <danielpeiro72@gmail.com>
2023-09-28 11:37:28 +0200

    Add Domain Events

--- Flex/Domain/Entity.cs
+++ Flex/Domain/Entity.cs
@@ -1,6 +1,21 @@
-﻿namespace Flex.Domain;
+﻿using MediatR;
+
+namespace Flex.Domain;
 
 public abstract class Entity<T> : IEntity<T> where T : notnull
 {
     public T Id { get; init; } = default!;
+
+    private readonly List<INotification> _events = new();
+    public IReadOnlyCollection<INotification> Events => _events.AsReadOnly();
+
+    public void AddEvent(INotification @event)
+    {
+        _events.Add(@event);
+    }
+
+    public void clearEvents()
+    {
+        _events.Clear();
+    }
 }
